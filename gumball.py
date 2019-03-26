7f8fc451ade6a31c113a1aee5c1d614ee97d0720
Emma Mack <emack@olin.edu>
2019-03-25 21:35:11 -0400

    Add layer switch test

--- gumball.py
+++ gumball.py
@@ -13,7 +13,7 @@ RED = (255, 0, 0)
 GREEN = ( 0, 255, 0)
 BLUE = ( 0, 0, 255)
 
-class Gumball:#encodes game state
+class Gumball:
     def __init__(self, x, y, color):
         self.x = x
         self.y = y
@@ -25,71 +25,89 @@ class Gumball:#encodes game state
     def move_down_layer():
         pass
 
-class one_gumball: #model class: encodes state of ball in the pygame
-    def __init__(self,height,width,x,y):
-        self.height = height
-        self.width = width
+class Quarter(Gumball):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
 
-    def play_gumball_animation():
-        pass
+def main():
+    pygame.init()
+    time = 0
 
-    def play_prize_animation():
-        pass
+    # set screen
+    screen = pygame.display.set_mode((1080, 980))
+    pygame.display.set_caption("Gumball Surprise")
 
-    def main():
-        pygame.init()
+    # time stuff
+    clock = pygame.time.Clock()
 
-        # set screen
-        screen = pygame.display.set_mode((1080, 720))
-        pygame.display.set_caption("Gumball Surprise")
-
-        # set background
-        # background = pygame.Surface(screenrect.size).convert()
-        # background.fill(BLACK)
-        # screen.blit(background, (0,0))
-        # pygame.draw.ellipse(background, GREEN, (300, 250, 40, 80), 1)
-        # pygame.display.update()
+    machine_l1 = pygame.image.load('gumball_layer_1.png')
+    machine_l2 = pygame.image.load('gumball_layer_2.png')
+    layers = [machine_l1, machine_l2]
 
 
-        # sprite stuff
-        # all_sprites = pygame.sprite.RenderUpdates()
+    # game loop
+    done = False
+    while not done:
 
-        # time stuff
-        clock = pygame.time.Clock()
+        #get input: exit game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                done = True
 
-        # game loop
-        done = False
-        while not done:
-
-            #get input: exit game
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-                    done = True
-
-            screen.fill(BLACK)
-            pygame.draw.ellipse(screen, GREEN, (300, 250, 40, 80), 1)
-
-            # clear sprites
-            # all_sprites.clear(screen,background)
-
-            # update all_sprites
-            # all_sprites.update()
-
-            # redraw sprites
-            # dirty = all_sprites.draw(screen)
-            # pygame.display.update(dirty)
-
-            # pygame.draw.ellipse(DISPLAYSURF, GREEN, (300, 250, 40, 80), 1)
-
-            # maintain frame rate
-            clock.tick(30)
-
-            pygame.display.update()
+        screen.fill(BLACK)
 
 
-    if __name__ == '__main__':
-        main()
-        pygame.display.quit()
-        exit()
+        if time == 100:
+            layers[0] = machine_l2
+            layers[1] = machine_l1
+
+        for image in layers:
+            screen.blit(image, (0,0))
+
+
+
+        # maintain frame rate
+        clock.tick(30)
+        time += 1
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
+    pygame.display.quit()
+    pygame.quit()
+    exit()
+
+
+
+
+
+
+# set background
+# background = pygame.Surface(screenrect.size).convert()
+# background.fill(BLACK)
+# screen.blit(background, (0,0))
+# pygame.draw.ellipse(background, GREEN, (300, 250, 40, 80), 1)
+# pygame.display.update()
+
+
+# sprite stuff
+# all_sprites = pygame.sprite.RenderUpdates()
+
+
+
+
+
+# clear sprites
+# all_sprites.clear(screen,background)
+
+# update all_sprites
+# all_sprites.update()
+
+# redraw sprites
+# dirty = all_sprites.draw(screen)
+# pygame.display.update(dirty)
+
+# pygame.draw.ellipse(DISPLAYSURF, GREEN, (300, 250, 40, 80), 1)
