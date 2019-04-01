ba4f1aa36fa1182c445a1b2d7179ba009acba714
Emma Mack <42980990+emmamack@users.noreply.github.com>
2019-04-01 16:53:01 -0400

    Delete animation.py

--- animation.py
+++
@@ -1,9 +0,0 @@
-def gumball_animation(time, t_start, layers):
-    t_since = time - t_start
-    gumball = layers[get_index(layers, Gumball)]
-
-    if  0 < t_since < 150:
-        gumball.x += 5
-        gumball.y += 5
-
-    return layers
